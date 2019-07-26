2d262faa87a75943a55db6ac5f425ff689b0a350
yche4 <yche4@iflytek.com>
2019-07-26 17:17:41 +0800

    添加ui

--- ui/src/consts.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = 'https://localhost:9010';
-const tokenCookie = 'accessToken';
-const tokenPrefix = 'Bearer ';
-const tokenExpiredInMinutes = '120min';
-
-export default {
-  baseUrl, tokenCookie, tokenPrefix, tokenExpiredInMinutes
-}
